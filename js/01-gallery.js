0b10fc8ce10c11a816c7081513885b1d3d4a05cf
Zbigniew-Klimczak <111083175+Zbigniew-Klimczak@users.noreply.github.com>
2022-12-21 13:30:11 +0100

    Update 01-gallery.js

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,19 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
-
-console.log(galleryItems);
+const gallery = document.querySelector(".gallery");
+console.log(galleryItems.length);
+for (let i = 0; i < galleryItems.length; i++) {
+  const item = document.createElement("div");
+  item.classList.add("gallery__item");
+  gallery.append(item);
+  const link = document.createElement("a");
+  link.href = galleryItems[i].original;
+  link.classList.add("gallery__link");
+  item.append(link);
+  const image = document.createElement("img");
+  image.classList.add("gallery__image");
+  image.src = galleryItems[i].preview;
+  image.alt = galleryItems[i].description;
+  image.dataset.source = galleryItems[i].original;
+  link.append(image);
+}
