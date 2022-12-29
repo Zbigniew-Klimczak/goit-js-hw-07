1fe27ebd58de27b8c28c120b2d9083e99660f149
Zbigniew-Klimczak <111083175+Zbigniew-Klimczak@users.noreply.github.com>
2022-12-29 04:29:25 +0100

    Task 2 done

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,7 +1,6 @@
 import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 const gallery = document.querySelector(".gallery");
-const galleryImg = document.querySelector(".gallery__image");
 for (let i = 0; i < galleryItems.length; i++) {
   const item = document.createElement("div");
   item.classList.add("gallery__item");
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,28 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
-
-console.log(galleryItems);
+const gallery = document.querySelector(".gallery");
+for (let i = 0; i < galleryItems.length; i++) {
+  const item = document.createElement("a");
+  item.href = galleryItems[i].original;
+  item.classList.add("gallery__item");
+  gallery.append(item);
+  const image = document.createElement("img");
+  image.classList.add("gallery__image");
+  image.src = galleryItems[i].preview;
+  image.alt = galleryItems[i].description;
+  item.append(image);
+}
+gallery.addEventListener("click", (evt) => {
+  evt.stopPropagation();
+  evt.preventDefault();
+  let simpleGallery = new SimpleLightbox(".gallery .gallery__item", {
+    captionsData: "alt",
+    captionDelay: 250,
+  });
+  if (evt.target.classList.contains("gallery__image")) {
+    simpleGallery.open();
+    simpleGallery.on("closed.simplelightbox", function () {
+      simpleGallery.destroy();
+    });
+  }
+});
